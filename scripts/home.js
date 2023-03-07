2b469b38898e5e3243dd3e4ed60f0fbe95092113
FarhanRA <farhanrizal11@gmail.com>
2023-03-08 03:03:37 +0700

    progress

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
-// function to convert unix time to formatted date YYYY-MM-DDThh:mm
-function formatted_date(time){
+// function to convert unix time to formatted datetime YYYY-MM-DDThh:mm
+function formatted_datetime(time){
 	const year = new Date(time).getFullYear();
     const month = (new Date(time).getMonth() + 1).toString().padStart(2, '0');
     const date = new Date(time).getDate().toString().padStart(2, '0');
@@ -12,6 +12,17 @@ function formatted_date(time){
 // 3 function to get user location
 var lat;
 var lon;
+var markers = {
+    'ori': {
+        'lat': '',
+        'lon': ''
+    },
+    'des': {
+        'lat': '',
+        'lon': ''
+    }
+};
+
 function get_userloc(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -24,15 +35,22 @@ function showPosition(position){
     alert('Found your location.');
     lat = position.coords.latitude;
     lon = position.coords.longitude;
+
+    document.getElementById('coor_ori1').value = lat;
+    document.getElementById('coor_ori2').value = lon;
+
+    console.log(lat, lon);
+    map.removeLayer(mark_ori);
+    mark_ori = L.marker([lat, lon]).addTo(map);
 }
 
 function showError(error){
     switch(error.code){
         case error.PERMISSION_DENIED:
-            alert('Please allow GPS access for this site.');
+            alert('Please allow GPS access for this site and try again.');
             break;
         case error.POSITION_UNAVAILABLE:
-            alert('Your location is unavailable.');
+            alert('Your location is unavailable. Check your internet and try again');
             break;
         case error.TIMEOUT:
             alert('Timed out. Proccess took too long.');
@@ -43,24 +61,45 @@ function showError(error){
     }
 }
 
+function select_coor_ori(){
+    map.removeLayer(mark_ori);
+    mark_ori = L.marker([lat, lon]).addTo(map);
+    document.getElementById('coor_ori1').value = lat;
+    document.getElementById('coor_ori2').value = lon;
+}
+
+function select_coor_des(){
+    map.removeLayer(mark_des);
+    mark_des = L.marker([lat, lon]).addTo(map);
+    document.getElementById('coor_dest1').value = lat;
+    document.getElementById('coor_dest2').value = lon;
+}
+
 // main function
 window.onload = () => {
+    // select position on map
+    map.on('click', (event) => {
+        lat = event.latlng.lat.toFixed(6);
+        lon = event.latlng.lng.toFixed(6);
+        
+        L.popup().setLatLng(event.latlng).setContent(
+            `
+                ${lat};${lon}
+                <div id="option_map">
+                    <button onclick="select_coor_ori()" class="btn_map">Set as Origin</button>
+                    <button onclick="select_coor_des()" class="btn_map">Set as Destination</button>
+                </div>
+        `).openOn(map);
+    });
+
     // Get date 10 day ahead range for routing
-    let today = formatted_date(Date.now());
-    let max_date = formatted_date(Date.now() + 10 * 86400000); // unix add 10 days
+    let today = formatted_datetime(Date.now());
+    let max_date = formatted_datetime(Date.now() + 10 * 24*60*60*1000); // unix add 10 days
     document.getElementById('time_dep').min = today;
     document.getElementById('time_dep').max = max_date;
 
-    // passing inputted data to result.html
-    document.getElementById('btn_search').onclick = () => {
-        let origin = (document.getElementById('coor_ori').value).replaceAll(' ', '');
-        let destination = document.getElementById('coor_dest').value.replaceAll(' ', '');
-        let datetime = document.getElementById('time_dep').value;
-
-        console.log(origin);
-        console.log(destination);
-        console.log(datetime);
-
-        window.open(`./html/result.html?${origin}&${destination}&${datetime}`, '_blank');
+    // searching user location
+    document.getElementById('btn_myloc').onclick = () => {
+        get_userloc();
     }
 }
--- scripts/result.js
+++ scripts/result.js
@@ -7,9 +7,10 @@
 */
 const urlSegment = window.location.search;
 const parameters = urlSegment.substring(urlSegment.indexOf('?')+1).split('&');
-const ori = parameters[0]; //lat,lon
-const dest = parameters[1]; //lat,lon
-const dep = parameters[2]; //YYYY-MM-DDThh:mm
+const ori = `${decodeURIComponent(parameters[0].slice(8))},${decodeURIComponent(parameters[1].slice(8))}`; //lat,lon
+const dest = `${decodeURIComponent(parameters[2].slice(8))},${decodeURIComponent(parameters[3].slice(8))}`; //lat,lon
+var dep = decodeURIComponent(parameters[4].slice(3)); //YYYY-MM-DDThh:mm
+var dep_unix = +new Date(dep); //13 dig number
 
 // function to get route from origin and destination, no limit
 async function get_route(){
@@ -17,81 +18,113 @@ async function get_route(){
     const fix_dest = dest.split(',').reverse().toString() //lon,lat
 
     const route_url = `http://router.project-osrm.org/route/v1/driving/${fix_ori};${fix_dest}?overview=false&steps=true`;
-    const response_1 = await fetch(route_url, {
-        method: 'GET'
-    }).catch(err => console.error(err));
+    const response_1 = await fetch(route_url, {method: 'GET'})
+        .catch(err => console.error(err));
 
     const route_data = await response_1.json();
+    if(route_data.code === 'NoRoute'){
+        alert("Sorry, Can't find the route. Go back and try different point")
+    }
     const steps = route_data.routes[0].legs[0].steps;
 
-    localStorage.setItem('route', route_data);
     show_results(steps);
 }
 
-// function for each waypoint to get city/district name, 2500/day & 1/sec
+// function, for each waypoint to get city/district name, 2500/day & 1/sec
 var address_list = [];
-async function get_name(loc, c){
-    const name_api_key = 'b1a8acdec4574fc98ec1b577ab778669';
+async function get_name(loc, c, pass_unix){//13 dig num
+    const name_api_key = 'b3bea85fe88146bf89d7c25c7c50f545';
     //b3bea85fe88146bf89d7c25c7c50f545
     //b1a8acdec4574fc98ec1b577ab778669
-    let name_url = `https://api.opencagedata.com/geocode/v1/json?q=${loc}/${step_time}&key=${name_api_key}&language=en&no_annotations=1&address_only=1&limit=1&no_record=1`;
+    let name_url = `https://api.opencagedata.com/geocode/v1/json?q=${loc}&key=${name_api_key}&language=en&no_annotations=1&address_only=1&limit=1&no_record=1&abbrv=1`;
     let response_2 = await fetch(name_url, {method: 'GET'})
         .catch(err => console.error(err));
 
     let name_data = await response_2.json();
-    let address = name_data.results[0].formatted.split(', ').slice(-4).join(', ').replace(/\s\d+/g, "").replace('unnamed road, ', '');
+
+    let village = name_data.results[0].components.village
+    let neighbourhood = name_data.results[0].components.neighbourhood
+    let quarter = name_data.results[0].components.quarter
+    let suburb = name_data.results[0].components.suburb
+    let city = name_data.results[0].components.city
+    let county = name_data.results[0].components.county
+    let district = name_data.results[0].components.state_district
+    let state = name_data.results[0].components.state
+    let region = name_data.results[0].components.region
+    let country = name_data.results[0].components.country
+
+    const list = [village, neighbourhood, quarter, suburb, city, county, district, state, region, country]
+    let pattern = /[^a-zA-Z0-9().,"'\/\s]/;
+    let str = '';
+    
+    for (let i = 0; i < 10; i++) {
+        if (!pattern.test(list[i])) {
+        str += `${list[i]}, `;
+        }
+    }
+
+    let address = str.replaceAll('undefined, ', '').slice(0, -2);
     console.log(address);
     
     if(!address_list.includes(address)){
         address_list.push(address);
         document.getElementById(`loc_${c}`).textContent = address;
 
-        let weather = await get_weather(address);
+        let weather = await get_weather(address, pass_unix, c);//13 dig num
         document.getElementById(`wea_${c}`).textContent = weather;
     }else{
-        document.getElementById(`coor_${c}`).remove();
+        document.getElementById(`root_${c}`).remove();
     }
 }
 
-// function for each city/district name to get weather, 1000/day
-async function get_weather(ad){
+// function, for each city/district name to get weather, 1000/day
+async function get_weather(ad, unix, c){//13 dig num
+    console.log(new Date(unix).toLocaleString())
     const weather_api_key = 'VZL3Q3HAS9A2H7BHMTH7YNLAW';
-    let weather_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ad}/next12days?unitGroup=metric&key=${weather_api_key}&iconSet=icons2&contentType=json&elements=cloudcover,dew,feelslike,humidity,icon,precip,pressure,snow,temp,visibility,winddir,windspeed`;
+    let weather_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ad}/${Math.round(unix/Math.pow(10,3))}?unitGroup=metric&key=${weather_api_key}&include=current&iconSet=icons2&contentType=json&elements=cloudcover,dew,feelslike,humidity,icon,precip,pressure,snow,snowdepth,temp,uvindex,visibility,winddir,windgust,windspeed`;
 
     let response_3 = await fetch(weather_url, {method: 'GET'})
         .catch(err => console.error(err));
-
     let weather_data = await response_3.json();
 
-    let icon = weather_data.days[0].hours[0].icon;
-    let precip = weather_data.days[0].hours[0].precip;
-    let temp = weather_data.days[0].hours[0].temp;
-    let windspeed = weather_data.days[0].hours[0].windspeed;
+    //LANJUT SINI
+    let ele = [];
+    Object.values(weather_data).forEach(value => {
+        ele.push(value);
+    });
 
-    // show icon weather
-    //document.getElementById('...').src = `./images/weather_icon/${icon}.png`;
+    /*
+        1cloudcover,2dew,3feelslike,4humidity,5*icon,6*precip,7pressure,8snow,9snowdepth,10*temp,11uvindex,12visibility,13winddir,14windgust,15*windspeed
+    */
+
+    document.getElementById(`ico_${c}`).src = `./images/weather_icon/${ele[4]}.png`;
 
     return `${icon}, precipitation: ${precip} mm, temperature: ${temp} C, wind: ${windspeed} km/h`;
 }
 
 // function to show the final result
 function show_results(s){
-    for(let counter=0; counter<s.length; counter++){
-        let lat = s[counter].maneuver.location[1];
-        let lon = s[counter].maneuver.location[0];
-        let datetime = 
+    for(let i=0; i<s.length; i++){
+        let lat = s[i].maneuver.location[1];
+        let lon = s[i].maneuver.location[0];
+        let distance = parseFloat(s[i].distance);
+        let time = ((distance/1000)/30)*60*60*1000; //format 13 dig number
+        dep_unix += time;
         
         document.getElementById('route_steps').innerHTML +=
             `
-                <div id="coor_${counter}" class="coor"></div>
-                <div id="loc_${counter}" class="loc"></div>
-                <div id="wea_${counter}" class="wea"></div>
+                <div id="root_${i}">
+                    <div id="coor_${i}" class="coor"></div>
+                    <div id="loc_${i}" class="loc"></div>
+                    <img id="ico_${i}" class="icon" src="" alt="icon"></img>
+                    <div id="wea_${i}" class="wea"></div>
+                </div>
             `;
         
-        console.log(counter)
-        get_name(`${lat},${lon}`, counter)
+        console.log(i)
+        get_name(`${lat},${lon}`, i, dep_unix)//13 dig num
         
-        document.getElementById(`coor_${counter}`).innerHTML = `<br>${lat}, ${lon}`;
+        document.getElementById(`coor_${i}`).innerHTML = `<br>${lat}, ${lon}<br>${new Date(dep_unix).toLocaleString()}`;
     }
 }
 
@@ -99,5 +132,5 @@ function show_results(s){
 window.onload = () => {
     document.getElementById('par').innerHTML = `${ori.split(',').reverse().toString()}<br>${dest.split(',').reverse().toString()}<br>${dep}`
 
-    //get_route();
+    get_route();
 }
